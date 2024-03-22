a36c3dd0a1bd70e21789416f3a0e7b6d77a46106
flou-ainan <flouainan@gmail.com>
2024-03-22 14:34:34 +0000

    Completed Lecture 9 | Components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+
+function CompA(){
+  return (
+    <>
+      <h1>CompA</h1>
+      <p>Hello Comp A</p>
+      <CompB />
+    </>
+  )
+          
+}
+function CompB(){
+  return (
+    <>
+      <h1>CompB</h1>
+      <p>Hello Comp A</p>
+    </>
+  )
+}
+
 export default function Home() {
   return (
     <>
      <h1>Hello World</h1>
+     <CompA />
     </>
   );
 }
