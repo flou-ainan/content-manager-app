753c4860ecc6722406bf7115f8ca5484e6640738
flou-ainan <flouainan@gmail.com>
2024-03-28 01:20:23 +0000

    Completed Lecture 26 | Section 4 | Resource Data

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import Layout from "components/Layout";
 import Newsletter from "components/Newsletter";
 import ResourceHihglight from "components/ResourceHihglight";
 import ResourceList from "components/ResourceList";
+import {resources} from "api/data"
 
 function Home() {
-
+  let list = resources.map
+  (item => (<><p>{JSON.stringify(item)}</p><hr/></>))
   return (
     <Layout>
       <ResourceHihglight />
       <Newsletter />
       <ResourceList />
+      {list}
     </ Layout>
   )
 }
